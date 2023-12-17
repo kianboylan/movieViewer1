bd88019852e25fc8a1598d5427636200d538dc25
kianboylan <kianboylan115@gmail.com>
2023-12-17 05:05:37 +0000

    Server & Local Hosting

--- db/migrate/20231212173800_update_users.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class UpdateUsers < ActiveRecord::Migration[7.1] # rubocop:todo Style/Documentation
-  def change
-    @u = User.find_by(email: 'admin@email.ie')
-    @u.update_attribute :admin, true
-  end
-end
